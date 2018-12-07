adc76b58a20a053c1d154e9fdf86c1a139054f4a
abedlubis <lubisabednego@gmail.com>
2018-12-07 17:00:58 +0700

    clear branch

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use((err, req, res, next) => {
+    console.log(err)
     res.status(err.status || 500)
     res.send({
       message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 'use strict'
+const employeeController = require('../controllers/employee')
 const express = require('express'),
       router = express.Router(),
       images = require('../helpers/images')
@@ -7,15 +8,18 @@ const express = require('express'),
 router.get('/', (req, res, next) => {
   res.send({ message: 'Welcome Buddy!' })
 })
+
 router.post('/upload',
   images.multer.single('image'), 
-  images.sendUploadToGCS,
-  (req, res) => {
+  images.sendUploadToGCS, (req, res) => {
     res.send({
       status: 200,
       message: 'Your file is successfully uploaded',
       link: req.file.cloudStoragePublicUrl
     })
-  })
+})
+
+router.get('/employee', employeeController.getEmployee)
+router.post('/render', employeeController.convertToBase64)
 
 module.exports = router
